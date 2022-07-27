45c4f0aa538022c96d54d61bb24d1350ed1bf57e
Flyman <icediv@163.com>
2022-07-27 14:13:04 +0800

    added Login Page

---
+++ loginpage/src/main/java/com/vazyme/loginpage/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.vazyme.loginpage.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
