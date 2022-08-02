34e5a37e6254f4f0f5629c1d29dde0e480957234
mingxing <icediv@163.com>
2022-08-02 11:09:47 +0800

    practice coroutine

--- kotlinlibrary/src/test/java/com/zmx/kotlinlibrary/ExampleUnitTest.kt
+++ kotlinlibrary/src/test/java/com/zmx/kotlinlibrary/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.zmx.kotlinlibrary
 
+import kotlinx.coroutines.*
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -12,6 +13,93 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
+
+//        GlobalScope.launch {
+//            println("hello")
+//            delay(1500)
+//            println("hello")
+//        }
+
+        runBlocking {
+            launch {
+                println("hello launch in runBlocking")
+                delay(1500)
+                println("hello first launch in runBlocking")
+            }
+
+            launch {
+                println("hello launch in runBlocking")
+                delay(1500)
+                println("hello second launch in runBlocking")
+            }
+
+            println("code in coroutine")
+            delay(1501)
+            println("code in coroutine")
+        }
+
+        Thread.sleep(1000)
+
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun `test coroutine`() {
+        val start = System.currentTimeMillis()
+
+        runBlocking {
+            repeat(100_000) {
+                launch {
+                    delay(1000)
+                    println("hello")
+                }
+            }
+        }
+        val end = System.currentTimeMillis()
+
+        println("cost time: ${end - start}")
+    }
+
+    @Test
+    fun `test coroutineScope`() {
+        runBlocking {
+            coroutineScope{
+                launch {
+                    for (i in 1..10) {
+                        println("i: $i")
+                        delay(1000)
+                    }
+                }
+                println("launch in coroutineScope")
+            }
+            println("code after coroutineScope")
+        }
+        println("runBlocking finish")
+
+//        coroutineScope{
+//            launch {
+//                for (i in 1..10) {
+//                    println("i: $i")
+//                    delay(1000)
+//                }
+//            }
+//            println("launch in coroutineScope")
+//        }
+        println("code after coroutineScope")
+    }
+
+    @Test
+    fun `coroutine with result`() {
+        val result = runBlocking {
+            val result = async {
+                delay(1000)
+                "hello"
+            }.await()
+            println("result: $result")
+            result
+        }
+        println("result: $result")
+    }
+
+
 }
