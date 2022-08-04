9f2c2d39fcd509f83df5fb62d0a662ef5df20357
mingxing <icediv@163.com>
2022-08-04 11:25:24 +0800

    try Ktor raw Socket - server

---
+++ libkotlin/src/main/kotlin/com/zmx/libkotlin/KtorSocketServer.kt
@@ -0,0 +1,32 @@
+package com.zmx.libkotlin
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.*
+
+// https://ktor.io/docs/servers-raw-sockets.html
+fun main(args: Array<String>) {
+    runBlocking {
+        val selectorManager = SelectorManager(Dispatchers.IO)
+        val serverSocket = aSocket(selectorManager).tcp().bind("127.0.0.1", 9002)
+        println("Server is listening at ${serverSocket.localAddress}")
+        while (true) {
+            val socket = serverSocket.accept()
+            println("Accepted $socket")
+            launch {
+                val receiveChannel = socket.openReadChannel()
+                val sendChannel = socket.openWriteChannel(autoFlush = true)
+                sendChannel.writeStringUtf8("Please enter your name\n")
+                try {
+                    while (true) {
+                        val name = receiveChannel.readUTF8Line()
+                        sendChannel.writeStringUtf8("Hello, $name!\n")
+                    }
+                } catch (e: Throwable) {
+                    socket.close()
+                }
+            }
+        }
+    }
+}
