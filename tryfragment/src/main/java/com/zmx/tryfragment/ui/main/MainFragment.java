5702ad967d4750300ddb211647e6b585a745e1f3
mingxing <icediv@163.com>
2022-08-09 19:37:49 +0800

    add onViewCreated and viewBinding

--- tryfragment/src/main/java/com/zmx/tryfragment/ui/main/MainFragment.java
+++ tryfragment/src/main/java/com/zmx/tryfragment/ui/main/MainFragment.java
@@ -8,11 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.zmx.tryfragment.R;
+import com.zmx.tryfragment.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
 
@@ -34,6 +36,18 @@ public class MainFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
+
+        Log.e("MainFragment", "onActivityCreated");
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        Log.e("MainFragment", "onViewCreated");
+
+        FragmentMainBinding binding = FragmentMainBinding.bind(view);
+
+        binding.message.setText("Hello World!");
+    }
 }
