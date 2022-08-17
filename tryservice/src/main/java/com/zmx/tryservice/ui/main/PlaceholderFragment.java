f3c74160313547708714f3c6b1ec3bdcf3e34493
mingxing <1308646+zhangmx@users.noreply.github.com>
2022-08-17 14:47:38 +0800

    rename PageViewModel to PlaceholderViewModel

--- tryservice/src/main/java/com/zmx/tryservice/ui/main/PlaceholderFragment.java
+++ tryservice/src/main/java/com/zmx/tryservice/ui/main/PlaceholderFragment.java
@@ -1,6 +1,5 @@
 package com.zmx.tryservice.ui.main;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,13 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.zmx.tryservice.R;
 import com.zmx.tryservice.databinding.FragmentMainBinding;
 import com.zmx.tryservice.services.MyIntentService;
 
@@ -28,14 +24,14 @@ public class PlaceholderFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private PageViewModel pageViewModel;
+    private PlaceholderViewModel placeholderViewModel;
     private FragmentMainBinding binding;
 
     private final Handler handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(@NonNull Message msg) {
 
-            pageViewModel.setText2(msg.obj.toString());
+            placeholderViewModel.setText2(msg.obj.toString());
 
             return true;
         }
@@ -52,12 +48,12 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+        placeholderViewModel = new ViewModelProvider(this).get(PlaceholderViewModel.class);
         int index = 1;
         if (getArguments() != null) {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
-        pageViewModel.setIndex(index);
+        placeholderViewModel.setIndex(index);
 
         Log.d("PlaceholderFragment", "onCreate");
     }
@@ -74,10 +70,10 @@ public class PlaceholderFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.sectionLabel;
-        pageViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        placeholderViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         final TextView textView2 = binding.textView;
-        pageViewModel.getText2().observe(getViewLifecycleOwner(), textView2::setText);
+        placeholderViewModel.getText2().observe(getViewLifecycleOwner(), textView2::setText);
 
         binding.button.setOnClickListener((view)->{
             Messenger messenger = new Messenger(handler);
--- tryservice/src/main/java/com/zmx/tryservice/ui/main/PlaceholderViewModel.java
+++ tryservice/src/main/java/com/zmx/tryservice/ui/main/PlaceholderViewModel.java
@@ -6,7 +6,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
-public class PageViewModel extends ViewModel {
+public class PlaceholderViewModel extends ViewModel {
 
     private MutableLiveData<Integer> mIndex = new MutableLiveData<>();
 
